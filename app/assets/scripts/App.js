0a276f8d6a9ff04cfb39aa74eb762cf1c38c5595
Julien Orjollet <oj.professionnel@gmail.com>
2022-03-30 22:41:08 +0200

    just commented javascript for now

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,17 +3,18 @@ import "lazysizes"
 
 import RevealOnScroll from "./modules/RevealOnScroll"
 
+/* No time to test and implement animations
+I just added some chunk of reusable code 
+but I will comment it out for matters of time
 TweenLite.set(".intro, .introHeading", { x: "-101%" })
 
 var lines = new TimelineMax()
-  .to(".introHeading", 1, { x: "0%" })
-  .to(".intro", 1, { x: "0%" }, "+=0.3")
+.to(".introHeading", 1, { x: "0%" })
+.to(".intro", 1, { x: "0%" }, "+=0.3")
 
 new RevealOnScroll(document.querySelectorAll(".reveal-img1"), 65)
 new RevealOnScroll(document.querySelectorAll(".reveal-img2"), 65)
-new RevealOnScroll(document.querySelectorAll(".service-item"), 75)
-new RevealOnScroll(document.querySelectorAll(".reveal-imgT"), 65)
-new MobileMenu()
+*/
 
 if (module.hot) {
   module.hot.accept()
